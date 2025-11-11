beb8808d15b148c2f457efc5540dfd785ba382ec
Juliana Gentile <jlg3012@g.rit.edu>
2025-11-11 16:10:14 -0500

    Replace RPM.py with Tachometer.py

---
+++ MSD_Work/Tachometer.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+from tkinter import ttk
+from Gauge import Gauge, GaugeConfig
+
+"""
+Tachometer.py
+
+Description:
+    This module creates the tachometer gauge component and defines
+    the functionality for it.
+
+Author: Juliana Gentile
+Date Created: 10/31/25
+Last Modified: 11/4/25
+
+Copyright (c) 2025 HotWheelz
+"""
+
